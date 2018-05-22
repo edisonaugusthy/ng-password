a39f0c9f0922422ef3482a60b5d82a3e141af135
Edison Augusthy <edisonaugusthy117@gmail.com>
2018-05-22 18:03:23 +0530

    updated functionalites

--- andro-angular-password-fields.js
+++ andro-angular-password-fields.js
@@ -1,60 +1,130 @@
-(function() {
 
-        'use strict';
-        var passApp = angular.module('andro-angular-password-fields', []);
-        
-        passApp.directive('changeType', directive);
-        directive.$inject = ['$timeout', '$rootScope'];
-        function directive($timeout, $rootScope) {
-            var directive = {
-                link: link,
-                restrict: 'A',
-                scope: {
-                    time: "=",
-                },
-            };
+var passApp = angular.module('andro-angular-password-fields', []);
 
-            return directive;
+(function () {
+    'use strict'
+    passApp.directive('passType', directive);
+    directive.$inject = ['$timeout', '$rootScope', '$compile'];
+    function directive($timeout, $rootScope, $compile) {
+        var directive = {
+            link: link,
+            restrict: 'A',
+        };
+        return directive;
+        function link(scope, element) {
+            var elm = element[0];
 
-            function link(scope, element) {
-                var timer = null;
-                
-                /* add your time here*/
-                var Tout = scope.time ? scope.time : 700;
-
-                var process = function() {
-                    setTimer();
-                    var elem = element[0];
-                    /* change type*/
-                    elem.value.length > 0 ?
-                        element[0].setAttribute("type", "text") : element[0].setAttribute("type", "password");
-                }
-
-                function setTimer() {
-                    timer = $timeout(function() {
-                        element[0].setAttribute("type", "password");
-                    }, Tout)
-
-                }
-
-                function clearTimer() {
-                    $timeout.cancel(timer);
-                }
-
-                element.bind('input', function() {
-                    clearTimer();
-                    process();
-                });
-
-                element.bind('keyup', function() {
-                    clearTimer();
-                    process();
-                });
-
-                scope.$on('$destroy', function() {
-                    $(element).unbind("input");
-                    $(element).unbind("keyup");
-                });
+            function activate() {
+                $(element).hide();
+                var newDirective = '<input type="text" change-type>';
+                $(elm).after($compile(newDirective)(scope));
+                $rootScope.$broadcast('ang-and-new-data-init', { data: elm.value });
             }
+
+            activate();
+            element.bind('focusin', function () {
+                activate();
+            });
+
+            scope.$on('ang-and-new-data-update', function (event, data) {
+                elm.value = data.data;
+                if (data.status) {
+                    element[0].setAttribute("type", "password");
+                    $(element).show();
+                }
+            })
+
         }
-    })()
+    }
+
+})();;
+
+(function () {
+    'use strict'
+    passApp.directive('changeType', directive);
+    directive.$inject = ['$timeout', '$rootScope', '$compile'];
+    function directive($timeout, $rootScope, $compile) {
+        var directive = {
+            link: link,
+            restrict: 'A',
+        };
+        return directive;
+
+        function link(scope, element) {
+            var orginalvalue = [];
+            var elem = element[0];
+            element.focus();
+
+
+            element.bind('keyup', function (event) {
+                if (event.which == 8) {
+                    orginalvalue.pop();
+                    elem.value = returnString(elem.value.length - 1);
+                    sendValue(orginalvalue, false)
+                }
+                else {
+                    calculate();
+                }
+            });
+
+            element.bind('focusout', function () {
+                focusOut();
+            });
+
+            function focusOut() {
+                var arr = [];
+                for (let index = 0; index < orginalvalue.length; index++) {
+                    arr.push('*');
+                }
+                elem.value = arr.join("");
+                sendValue(orginalvalue, true)
+            }
+
+            function calculate() {
+                if (elem.value.length > 1) {
+                    orginalvalue.push(elem.value[elem.value.length - 1]);
+                    elem.value = returnString(elem.value.length - 1)
+                    sendValue(orginalvalue, false)
+                }
+                else {
+                    orginalvalue.push(elem.value);
+                    sendValue(orginalvalue, false)
+                }
+
+            }
+
+            function returnString(len) {
+                var arr = [];
+                for (let index = 0; index < len; index++) {
+                    arr.push('*')
+                }
+                arr.push(orginalvalue[orginalvalue.length - 1]);
+                return arr.join("");
+
+            }
+            scope.$on('$destroy', function () {
+                $(element).unbind("input");
+            });
+
+            function sendValue(data, state) {
+                $rootScope.$broadcast('ang-and-new-data-update', { data: orginalvalue.join(""), status: state });
+                if (state) {
+                    $(element).remove();
+                }
+
+            }
+
+            scope.$on('ang-and-new-data-init', function (event, data) {
+                if (data.data.length > 0) {
+                    for (let index = 0; index < data.data.length; index++) {
+                        orginalvalue.push(data.data[index]);
+                    }
+                    elem.value = returnString(data.data.length)
+                    sendValue(orginalvalue, false)
+                }
+            })
+
+        }
+    }
+
+})();;
